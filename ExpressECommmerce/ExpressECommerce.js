070bb1af3695617aace6d96aaccd884a84c568b3
wufo121 <wufo121@gmail.com>
2024-11-15 09:59:55 +0100

    first commit to init my project

---
+++ ExpressECommmerce/ExpressECommerce.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const port = 3000;
+
+app.get("/", (req, res) => {
+   const connection = mysql.createConnection({
+      host: "localhost",
+      user: "eCommerce",
+      password: "test",
+      database: "eCommerce",
+   });
+
+   connection.connect((err) => {
+      if (err) {
+         console.error("erreur de connexion : " + err.stack);
+         return;
+      }
+   });
+
+   connection.query("SELECT * FROM Article", (err, rows, fields) => {
+      if (err) throw err;
+      res.json(rows);
+   });
+});
+
+app.listen(port, () => {
+   console.log(`Example app listening on port ${port}`);
+})
