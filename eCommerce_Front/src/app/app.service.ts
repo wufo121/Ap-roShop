02c0cb4022880b62b039f84c97b73377ef3b3dfb
wufo121 <wufo121@gmail.com>
2024-12-12 19:40:48 +0100

    Update: Add token with jwt and get data of curentUser

--- eCommerce_Front/src/app/app.service.ts
+++ eCommerce_Front/src/app/app.service.ts
@@ -20,20 +20,35 @@ export class AppService {
     return this.http.get<any[]>('/api/articles');
   }
 <<<<<<< HEAD
+<<<<<<< HEAD
 
   getCurrentUser(): Observable<any> {
     const token = localStorage.getItem('currentUser');
     console.log('Token retrieved:', token);
+=======
+
+  getCurrentUser(): Observable<any> {
+    const token = localStorage.getItem('currentUser');
+>>>>>>> d5d3dce (Update: Add token with jwt and get data of curentUser)
 
     if (!token) {
       return new Observable<any>();
     }
 
+<<<<<<< HEAD
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
     return this.http.get<any>('api/me', { headers });
 =======
   isLoggedIn(): boolean {
     return localStorage.getItem('curentUser') !== null;
 >>>>>>> ecf55a6 (Update: we can see username when user is login)
+=======
+    const parsedToken = JSON.parse(token);
+    const headers = new HttpHeaders().set(
+      'Authorization',
+      `Bearer ${parsedToken}`
+    );
+    return this.http.get<any>('api/me', { headers });
+>>>>>>> d5d3dce (Update: Add token with jwt and get data of curentUser)
   }
 }
